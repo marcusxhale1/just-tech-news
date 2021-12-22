8204d894be869eeece9686ab8627772b14c06980
Marcus Hale <marcusxhale1@gmail.com>
2021-12-21 18:24:29 -0800

    EVERYTHING IS CONNECTED, FINALLLYYYYY

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
-// Importing sequlize into the app now
+// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
 // create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequelize('just_tech_news_db', 'username', 'password', {
+const sequelize = new Sequelize('just_tech_news_db', 'root', '@Xavier92939988', {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306
