377debcd11fb107c48fa122ea95a0c4f6ecf5586
Marcus Hale <marcusxhale1@gmail.com>
2021-12-21 18:44:36 -0800

    well you know, I figured out the bug, and I'm really starting to comprehend what's going on

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,33 +3,36 @@ const { User } = require('../../modles');
 
 // GET /api/users
 router.get('/', (req, res) => {
-    // Access our User model and run .findAll() method)
-    User.findAll()
-      .then(dbUserData => res.json(dbUserData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
+    // Access our User model and run .findAll() method) ALSO EXCLUDING PASSWORD TO BE SHOWN 
+    User.findAll({
+        attributes: { exclude: ['password'] }
+      })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
   });
 
 // GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
-      where: {
-        id: req.params.id
-      }
-    })
-      .then(dbUserData => {
-        if (!dbUserData) {
-          res.status(404).json({ message: 'No user found with this id' });
-          return;
+        attributes: { exclude: ['password'] },
+        where: {
+          id: req.params.id
         }
-        res.json(dbUserData);
       })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
+        .then(dbUserData => {
+          if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+          }
+          res.json(dbUserData);
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
   });
 
 // POST /api/users
