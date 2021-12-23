db142eac9ac9c2b90c190ed9e6fcdde4e89241a0
Marcus Hale <marcusxhale1@gmail.com>
2021-12-22 23:18:58 -0800

    these are the routes with everything working including the voting

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const { Post, User } = require('../../modles');
+const { Post, User, Vote } = require('../../modles');
 const { post } = require('./user-routes');
+const sequelize = require('../../config/connection');
 
 // We are getting all users now 
 
@@ -62,6 +63,35 @@ router.get('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
+  });
+
+  // PUT api/posts/upvote
+  router.put('/upvote', (req, res) => {
+    Vote.create({
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    })
+    .then(() => {
+        // then find the post we just voted on
+        return Post.findOne({
+          where: {
+            id: req.body.post_id
+          },
+          attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            // use raw MySQL aggregate function query to get a count of how many votes the post has and return it under the name `vote_count`
+            [
+              sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+              'vote_count'
+            ]
+          ]
+        })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => res.json(err));
+    });
   });
 
   //This is the Put route 
