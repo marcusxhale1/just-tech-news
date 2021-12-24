aa9dce2822fe5080fce0f9499f9639887b548e05
Marcus Hale <marcusxhale1@gmail.com>
2021-12-23 19:25:32 -0800

    connect to jawsDB

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,16 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our db
-const sequelize = new Sequelize('just_tech_news_db', 'root', '@Xavier92939988', {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
-});
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize('just_tech_news_db', 'root', '@Xavier92939988', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+}
 
 module.exports = sequelize;
